2aa7cf7cc098edd26200a5e3e5dd2d6dc30a2c37
treenod-sally <songyi@treenod.com>
2023-10-03 15:32:49 +0900

    ✨ create

--- src/App.js
+++ src/App.js
@@ -36,18 +36,34 @@ function Article(props) {
   </article>
 }
 
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title,body);
+    }}>
+     <p> <input type="text" name="title" placeholder='title'/> </p>
+     <p><textarea name="body" placeholder="body"></textarea> </p>
+     <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
+
 function App() {
   // const _mode = useState('WELCOME'); //초기값
   // const mode = _mode[0]; //읽을때 0번째 원소
   // const setMode = _mode[1]; //값을 바꿀때는 1번째 원소
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics,setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
-  ]
+  ])
   let content = null;
   if(mode==='WELCOME') {
     content = <Article title="Welcome" body="Hello.WEB"></Article>
@@ -61,6 +77,16 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+  }else if (mode === 'CREATE') {
+    content = <Create onCreate={(_title,_body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic)
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
   }
   return (
     <div>
@@ -72,6 +98,10 @@ function App() {
         setId(_id);
       }} ></Nav>
       {content}
+      <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>CREATE</a>
     </div>
   );
 }
