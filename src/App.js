f0cb53fa703103bb4bdbb5825089007817722866
treenod-sally <songyi@treenod.com>
2023-10-17 23:46:52 +0900

    ✨ 배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,182 +1,64 @@
-import {useState} from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
-import Counter from './Counter';
-import InputSample from './InputSample';
-
-function Header(props) {
-  console.log('props',props.title)
-  return  <header>
-    <h1><a href="/" onClick={(event)=>{
-        event.preventDefault();
-        props.onChangeMode();
-    }}>{props.title}</a></h1>
-  </header>
-}
-
-function Nav(props) {
-  const lis = [
-  ]
-  for (let i=0; i<props.topics.length; i++){
-    let t = props.topics[i]
-    lis.push( <li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
-        event.preventDefault();
-        props.onChangeMode(Number(event.target.id))
-      }}>{t.title}</a>
-      </li>)
-  }
-  return <nav>
-  <ol>
-   {lis}
-  </ol>
-</nav>
-}
-
-function Article(props) {
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
-}
-
-function Create(props) {
-  return <article>
-    <h2>Create</h2>
-    <form onSubmit={event=>{
-      event.preventDefault();
-      const title = event.target.title.value;
-      const body = event.target.body.value;
-      props.onCreate(title,body);
-    }}>
-     <p> <input type="text" name="title" placeholder='title'/> </p>
-     <p><textarea name="body" placeholder="body"></textarea> </p>
-     <p><input type="submit" value="Create"></input></p>
-    </form>
-  </article>
-}
-
-function Update(props) {
-  const [title, setTitle] = useState(props.title)
-  const [body,setBody] = useState(props.body)
-  return <article>
-    <h2>Update</h2>
-    <form onSubmit={event=>{
-      event.preventDefault();
-      const title = event.target.title.value;
-      const body = event.target.body.value;
-      props.onUpdate(title,body);
-    }}>
-     <p> <input type="text" name="title" value={title} onChange={(event=>{
-      setTitle(event.target.value);
-     })}/> </p>
-     <p><textarea name="body" value={body} onChange={event=>{
-      setBody(event.target.value);
-     }}></textarea> </p>
-     <p><input type="submit" value="Update"></input></p>
-    </form>
-  </article>
-}
+import {useState, useRef} from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  // const _mode = useState('WELCOME'); //초기값
-  // const mode = _mode[0]; //읽을때 0번째 원소
-  // const setMode = _mode[1]; //값을 바꿀때는 1번째 원소
-  const [mode, setMode] = useState('WELCOME');
-  const [id, setId] = useState(null);
-  const [nextId, setNextId] = useState(4);
-  const [topics,setTopics] = useState([
-    {id:1, title:'html', body:'html is ...'},
-    {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'javascript', body:'javascript is ...'}
-  ])
-  let content = null;
-  let contextControl = null;
-  if(mode==='WELCOME') {
-    content = <Article title="Welcome" body="Hello.WEB"></Article>
-  }else if (mode === 'READ') {
-    let title, body = null;
-    for (let i=0; i<topics.length; i++){
-      console.log(topics[i].id, id)
-      if (topics[i].id === id) {
-        title = topics[i].title;
-        body = topics[i].body;
-      }
-    }
-    content = <Article title={title} body={body}></Article>
-    contextControl = <>
-    <li><a href={'/update/'+id} onClick ={event=>{
-      event.preventDefault();
-      setMode('UPDATE');
-    }}>Update</a></li>
-    <li><input type="button" value="Delete" onClick={()=>{
-      const newTopics = []
-      for(let i=0;i<topics.length;i++){
-        if(topics[i].id !== id){
-          newTopics.push(topics[i]);
-        }
-      }
-      setTopics(newTopics);
-      setMode('WELCOME');
-    }}/></li>
-    </>
-  }else if (mode === 'CREATE') {
-    content = <Create onCreate={(_title,_body)=>{
-      const newTopic = {id:nextId, title:_title, body:_body}
-      const newTopics = [...topics]
-      newTopics.push(newTopic)
-      setTopics(newTopics);
-      setMode('READ');
-      setId(nextId);
-      setNextId(nextId+1);
-    }}></Create>
-  }else if (mode === 'UPDATE'){
-    let title, body = null;
-    for (let i=0; i<topics.length; i++){
-      console.log(topics[i].id, id)
-      if (topics[i].id === id) {
-        title = topics[i].title;
-        body = topics[i].body;
-      }
-    }
-    content = <Update title={title} body={body} onUpdate={(title,body)=>{
-      console.log(title,body);
-      const newTopics = [...topics]
-      const updatedTopic = {id:id, title:title, body:body}
-      for(let i=0; i <newTopics.length;i++ ){
-        if(newTopics[i].id === id){
-          newTopics[i]= updatedTopic
-          break;
-        }
-      }
-      setTopics(newTopics);
-      setMode('READ');
-    }}></Update>
+  const [inputs, setInputs] = useState({
+    username:'',
+    email:''
+  })
+  const {username, email} = inputs; //비구조화 할당을 통해 값 추출 
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs, //spread문법 = 불변성을 지킨다 => 불변성을 지켜주어야만 리액트 컴포넌트에서 상태가 업데이트 되었음을 감지할 수 있고 이에 따라 필요한 리렌더링을 진행
+      [name]:value,
+    })
   }
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+      const user = {
+        id: nextId.current,
+        username,
+        email
+      }
+      setUsers([...users,user]) //spread 연산자 사용 혹은
+      setUsers(users.concat(user)); //concat 사용
+
+      setInputs({
+        username:'',
+        email:''
+      })
+      nextId.current += 1;
+  }
+
   return (
     <div>
-      <Header title="REACT" onChangeMode={()=>{
-        setMode('WELCOME');
-      }}></Header>
-      <Nav topics={topics} onChangeMode={(_id)=>{
-        setMode('READ');
-        setId(_id);
-      }} ></Nav>
-      {content}
-      <ul>
-        <li><a href="/create" onClick={event=>{
-          event.preventDefault();
-          setMode('CREATE');
-        }}>CREATE</a></li>
-        {contextControl}
-      </ul>
-      <Wrapper>
-        {/* true는 자바스크립트 값이기때문에 중괄호로 감싸주어야함 */}
-        {/* props 이름만 작성하고 값 설정을 생략한다면 true */}
-        <Hello color="green" name="sally" isSpecial />
-      </Wrapper>
-      <Counter />
-      <InputSample />
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users}></UserList>
     </div>
   );
 }
